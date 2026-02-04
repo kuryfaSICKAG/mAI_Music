6751fdc5b0ba99029040b63bf8d428a29fa1330d
Bernd <bernd.keser@gmail.com>
2026-02-04 10:18:27 +0100

    Added exported function to search for tracks

---
+++ services/service.ts
@@ -0,0 +1,38 @@
+import { DeezerAPI } from "../apiServices/deezerAPI/deezer.ts";
+
+
+export async function searchSong(track: string){
+    const api = new DeezerAPI();
+    const tracks = await api.searchTrack(track);
+    if (tracks.data && tracks.data.length > 0) {
+        console.log("Tracks found:");
+        tracks.data.forEach((track: any, idx: number) => {
+        console.log(`Track #${idx + 1}:`);
+        console.log(`  Title: ${track.title || track.name || track.title_short}`);
+        if (track.artist && track.artist.name) {
+            console.log(`  Artist: ${track.artist.name}`);
+        } else if (track.artist) {
+            console.log(`  Artist: ${track.artist}`);
+        } else if (track.artist_name) {
+            console.log(`  Artist: ${track.artist_name}`);
+        }
+        if (track.album && track.album.title) {
+            console.log(`  Album: ${track.album.title}`);
+        } else if (track.album) {
+            console.log(`  Album: ${track.album}`);
+        } else if (track.album_title) {
+            console.log(`  Album: ${track.album_title}`);
+        }
+        if (track.duration) {
+            console.log(`  Duration: ${track.duration} seconds`);
+        }
+        if (track.id) {
+            console.log(`  ID: ${track.id}`);
+        }
+        console.log('---');
+        });
+    } else {
+    console.log("No tracks found.");
+  }
+}
+searchSong("Example Song");
