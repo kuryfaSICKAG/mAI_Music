21e9354067682bc10052326c9e0cfc67bebc1422
Bernd <bernd.keser@gmail.com>
2026-03-27 09:39:29 +0100

    toten code entfernt

--- Server/start.ts
+++ Server/start.ts
@@ -3,12 +3,8 @@ import { searchRouter } from "./routes/search.ts";
 import { rootRouter } from "./routes/root.ts";
 import { usersRouter } from "./routes/users.ts";
 import { authRouter } from "./routes/auth.ts";
-import { profileRouter } from "./routes/profile.ts";
-import { favoritesRouter } from "./routes/favorites.ts";
 import { playlistsRouter } from "./routes/playlists.ts";
-import { recommendationsRouter } from "./routes/recommendations.ts";
 import { onlineRouter } from "./routes/online.ts";
-import { opsRouter } from "./routes/ops.ts";
 
 const app = express();
 const PORT = 8080;
@@ -20,12 +16,8 @@ app.use(rootRouter);
 app.use(searchRouter);
 app.use(usersRouter);
 app.use(authRouter);
-app.use(profileRouter);
-app.use(favoritesRouter);
 app.use(playlistsRouter);
-app.use(recommendationsRouter);
 app.use(onlineRouter);
-app.use(opsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server läuft unter http://localhost:${PORT}`);
